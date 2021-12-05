0fe0437c198eeb6871580ea8e42c1346408209fe
eanderson <anderson.erica.lee@gmail.com>
2021-12-04 23:56:53 -0800

    get user by ID

--- src/main/java/not/nerds/secretsantaservice/contoller/UserController.java
+++ src/main/java/not/nerds/secretsantaservice/contoller/UserController.java
@@ -1,16 +1,10 @@
 package not.nerds.secretsantaservice.contoller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import not.nerds.secretsantaservice.data.entity.User;
 import not.nerds.secretsantaservice.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -19,7 +13,12 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getUsers() throws JsonProcessingException {
+    public Iterable<User> getUsers() {
         return this.userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return this.userRepository.findById(id).get();
     }
 }
